28f2c32d4d22278278a5706768000aa4f598eb8d
w3qiu <damon.q26@gmail.com>
2020-02-27 13:32:15 -0800

    finish lab8

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,15 @@
 function initMap() {
 	// add your code here
+    L.mapquest.key = '5XGGuWTV3KhLtP9k1i7Wka0ukO3v5rvD';
+
+    // 'map' refers to a <div> element with the ID map
+    var map = L.mapquest.map('map', {
+      center: [32.878958, -117.235924],
+      layers: L.mapquest.tileLayer('map'),
+      zoom: 12,
+    zoomControl: false
+    });
+    
+    L.marker([32.878958, -117.235924]).addTo(map);
+    
 }
